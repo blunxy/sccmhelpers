ad5c4d80ad9b78f4aefa54dcb175da3993e3252e
Jordan @ Work <jpratt@mtroyal.ca>
2015-06-29 11:51:04 -0600

    Initial commit with nuke-allcsisdeployments.

---
+++ Nuke-AllCSISDeployments.ps1
@@ -0,0 +1,7 @@
+﻿if ($ourdeps -eq $null) {
+    $ourdeps = Get-CMDeployment | where {$_.SoftwareName -match "^CSIS" }
+}
+
+foreach ($dep in $ourdeps) {
+    Remove-CMDeployment -ApplicationName $dep.SoftwareName -DeploymentId $dep.DeploymentID -Force
+}
