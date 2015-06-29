da5d9967b3a040ffeb025215746bba44b27ba10f
Jordan @ Work <jpratt@mtroyal.ca>
2015-06-29 11:54:42 -0600

    Add quick documentation to Nuke

--- Nuke-AllCSISDeployments.ps1
+++ Nuke-AllCSISDeployments.ps1
@@ -1,4 +1,14 @@
-﻿if ($ourdeps -eq $null) {
+﻿<#
+    This roughly grabs all deployments on the MRU SCCM server and nukes them
+    if the software in the deployments starts with CSIS. 
+
+    Two issues with this (at least!):
+      1) If the app doesn't start with CSIS, well boo-hoo.
+      2) Even if the app is being deployed in somebody else's collection (hi, IT!),
+      it's gonna go buh-bye.
+#>
+
+if ($ourdeps -eq $null) {
     $ourdeps = Get-CMDeployment | where {$_.SoftwareName -match "^CSIS" }
 }
 
