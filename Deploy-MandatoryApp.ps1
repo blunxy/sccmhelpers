2e71625e142e1a97896e0f39e81880a0cda0e9f8
Jordan @ Work <jpratt@mtroyal.ca>
2015-06-29 16:42:28 -0600

    Deploy and Uninstall scripts up.

---
+++ Deploy-MandatoryApp.ps1
@@ -0,0 +1,14 @@
+﻿<#
+    Create a mandatory deployment that starts right away.
+#>
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$TargetCollection,
+
+    [Parameter(Mandatory=$True)]
+    [string]$AppName
+)
+$AvailableTime = Get-Date
+$DeploymentTime = $AvailableTime.AddSeconds(1)
+
+Start-CMApplicationDeployment -CollectionName $TargetCollection -Name $AppName -DeployAction Install -DeployPurpose Required -UserNotification DisplayAll -AvaliableTime $AvailableTime -DeadlineTime $DeploymentTime
---
+++ Uninstall-MandatoryApp.ps1
@@ -0,0 +1,14 @@
+﻿<#
+    Create a mandatory uninstall deploy that starts right away.
+#>
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$TargetCollection,
+
+    [Parameter(Mandatory=$True)]
+    [string]$AppName
+)
+$AvailableTime = Get-Date
+$DeploymentTime = $AvailableTime.AddSeconds(1)
+
+Start-CMApplicationDeployment -CollectionName $TargetCollection -Name $AppName -DeployAction Uninstall -DeployPurpose Required -UserNotification DisplayAll -AvaliableTime $AvailableTime -DeadlineTime $DeploymentTime
