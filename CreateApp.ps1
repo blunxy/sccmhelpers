9af8b7dc6a750888df5185c52e156b718bf7eea8
$jpratt <$jpratt@mtroyal.ca>
2016-08-18 12:38:10 -0600

    Add detection script and content distribution.

--- CreateApp.ps1
+++ CreateApp.ps1
@@ -1,24 +1,54 @@
-﻿$AppParams = @{
-    'Name' = '@Goo';
-    'Owner' = 'Jordan P';
-    'SoftwareVersion' = '12.3.0';
+﻿Function GetAppRegistryKey($AppName, $AppVersion)
+{
+    $TweakedAppName = $AppName.Replace("-","")
+    $TweakedAppName = $TweakedAppName.Replace(" ",".")
+    $TweakedAppName = $TweakedAppName.ToLower()
+    "$TweakedAppName.$AppVersion"
 }
 
-$DeploymentParams = @{
+$APP_NAME = "_ZoomIt"
+$APP_VERSION = "4.5"
+$APP_REGKEY = GetAppRegistryKey $APP_NAME $APP_VERSION
+$CONTENT_LOCATION = "\\ad\empl\IT\MergeInstall1\Utils\csis-bats\+install-script-repo\$APP_REGKEY"
 
-    'DeploymentTypeName' = 'MACO-install-script'
-    'InstallCommand' = 'install.cmd';
-    'ScriptLanguage' = 'Powershell'
-    'ScriptText' = 'some PS script for detection?';
-    'ContentLocation' = '\\ad\empl\IT\MergeInstall1\Software PQRS\Steem\steem.2.5b.7\';
-    'EstimatedRunTimeMins' = 20;
-    'InstallationBehaviorType' = 'InstallForSystem';
-    'LogonRequirementType' = 'WhereOrNotUserLoggedOn';
-    'MaximumRuntimeMins' = 90;    
+#### Create a new app
+
+
+$AppParams = @{
+    'Name' = $APP_NAME;
+    'Owner' = 'Jordan P';
+    'SoftwareVersion' = $APP_VERSION;
 }
 
 $Newapp = New-CMApplication @AppParams
 
 Move-CMObject -FolderPath 'MTR:\Application\CompSci' -InputObject $NewApp
 
-Add-CMScriptDeploymentType @DeploymentParams -ApplicationName $Newapp.LocalizedDisplayName
+
+#### Set up Deployment
+
+
+$DeploymentParams = @{
+
+    'DeploymentTypeName' = 'MACO-install-script'
+    'InstallCommand' = 'install.cmd';
+    'ScriptLanguage' = 'VBScript'
+    'ContentLocation' = $CONTENT_LOCATION;
+    'EstimatedRunTimeMins' = 20;
+    'InstallationBehaviorType' = 'InstallForSystem';
+    'LogonRequirementType' = 'WhereOrNotUserLoggedOn';
+    'MaximumRuntimeMins' = 90;    
+}
+
+
+$DetectionScript = @"
+Set oReg = GetObject("winmgmts:{impersonationLevel=impersonate}!\\.\root\default:StdRegProv")
+
+If oReg.EnumKey(, "Software\CSISCustomInstalls\${APP_REGKEY}", "") = 0 Then
+  wspcript.echo "Key Exists"
+End If
+"@
+
+Add-CMScriptDeploymentType @DeploymentParams -ApplicationName $Newapp.LocalizedDisplayName -ScriptText $DetectionScript
+
+Start-CMContentDistribution -ApplicationName $APP_NAME -DistributionPointGroupName 'MRU'
